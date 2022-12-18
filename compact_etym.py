c4c26ee895f2b3bf7eb485f185ca5fd18f5f58aa
Wang Guosheng <wguosh@gmail.com>
2022-12-18 18:00:58 +0800

    edit regex

--- compact_etym.py
+++ compact_etym.py
@@ -11,22 +11,26 @@ args = parser.parse_args()
 file = args.file[0]
 
 with open(file, 'r') as f:
-    data = "".join(f.readlines())
-    data = re.sub(r"###\s*Etymology((\n([^#]+#)*[^#]*)*)\n#",
-                  # lambda m: print(m),
-                  lambda m: "### Etymology\n" +
-                  m.group(1).replace('\n', '') + "\n#",
-                  data)
-    data = re.sub(r"####\s*Derived\s[Tt]erms((\n([^#]+#)*[^#]*)*)\n#",
-                  # lambda m: print(m),
-                  lambda m: ("#### Derived terms\n" +
-                                    m.group(1).replace('\n-', ',') + "\n#").replace('\n,', '\n'),
-                  data)
-    data = re.sub(r"####\s*Descendants((\n([^#]+#)*[^#]*)*)\n#",
-                  # lambda m: print(m),
-                  lambda m: ("#### Descendants\n" +
-                                    m.group(1).replace('\n-', ',') + "\n#").replace('\n,', '\n'),
-                  data)
+  data = "".join(f.readlines())
+  data = re.sub(r"###\s*Etymology((\n([^#]+#)*[^#]*)*)\n#",
+                # lambda m: print(m),
+                lambda m: "### Etymology\n" +
+                m.group(1).replace('\n', ' ') + "\n#",
+                data)
+  data = re.sub(r"####\s*Derived\s[Tt]erms((\n([^#]+#)*[^#]*)*)\n#",
+                # lambda m: print(m),
+                lambda m: "#### Derived terms\n" + \
+                re.sub(r'^\,\s*', '',
+                       m.group(1).replace('\n-', ', ').replace('\n', '')
+                       ) + "\n#",
+                data)
+  data = re.sub(r"####\s*Descendants((\n([^#]+#)*[^#]*)*)\n#",
+                # lambda m: print(m),
+                lambda m: "#### Descendants\n" + \
+                re.sub(r'^\,\s*', '',
+                       m.group(1).replace('\n-', ', ').replace('\n', '')
+                       ) + "\n#",
+                data)
 
 with open(file, 'w') as f:
-    f.write(data)
+  f.write(data)
